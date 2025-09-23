48cb2ff9dbcae15e547dbe46e7b46ad254c94e4c
Josue lozada <129800585+JosueLozada08@users.noreply.github.com>
2025-09-22 21:17:53 -0500

    vista crear y metodos get y put

--- Crud.Net8/Controllers/InicioController.cs
+++ Crud.Net8/Controllers/InicioController.cs
@@ -3,6 +3,8 @@ using Crud.Net8.Models;
 using Microsoft.AspNetCore.Mvc;
 using Crud.Net8.Datos;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Crud.Net8.Controllers
 {
@@ -18,12 +20,29 @@ namespace Crud.Net8.Controllers
         {
             _contexto = contexto;
         }
-
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await _contexto.Contacto.ToArrayAsync());
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Contacto.Add(contacto);
+                await _contexto.SaveChangesAsync();//se convierte en un metodo asincrono y se guarda 
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
